ffaefa5c1e5fef7b9fc7fe8a018dc9c5041c8f55
smallbraintime <patrykstruzek@proton.me>
2025-02-17 15:10:12 +0100

    Add day1 part2, minor fixes

--- day1/main.go
+++ day1/main.go
@@ -10,14 +10,21 @@ import (
 	"sync"
 )
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 type LocationList struct {
 	left, right []int
 }
 
-func readLocations(path string) (*LocationList, error) {
+func readLocations(path string) (LocationList, error) {
 	buf, err := os.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return LocationList{nil, nil}, err
 	}
 	lines := strings.Split(string(buf), "\n")
 
@@ -30,21 +37,21 @@ func readLocations(path string) (*LocationList, error) {
 
 		ln, err := strconv.Atoi(words[0])
 		if err != nil {
-			return nil, err
+			return LocationList{nil, nil}, err
 		}
 
 		rn, err := strconv.Atoi(words[1])
 		if err != nil {
-			return nil, err
+			return LocationList{nil, nil}, err
 		}
 
 		left = append(left, ln)
 		right = append(right, rn)
 	}
-	return &LocationList{left, right}, nil
+	return LocationList{left, right}, nil
 }
 
-func sortLocations(list *LocationList) {
+func sortLocations(list LocationList) {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
@@ -61,7 +68,7 @@ func sortLocations(list *LocationList) {
 	wg.Wait()
 }
 
-func sumLocationDistances(list *LocationList) int {
+func sumLocationDistances(list LocationList) int {
 	var sum int
 	for i := 0; i < len(list.left); i++ {
 		sum += int(math.Abs(float64(list.left[i] - list.right[i])))
@@ -69,12 +76,60 @@ func sumLocationDistances(list *LocationList) int {
 	return sum
 }
 
+func countBothOccurences(list LocationList) map[int]int {
+	leftOccurences := make(map[int]int)
+	rightOccurences := make(map[int]int)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for _, v := range list.left {
+			leftOccurences[v] += 1
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for _, v := range list.right {
+			rightOccurences[v] += 1
+		}
+	}()
+
+	wg.Wait()
+
+	var occurences = make(map[int]int)
+	for k, lv := range leftOccurences {
+		rv, ok := rightOccurences[k]
+		if ok {
+			occurences[k] = max(lv, rv)
+		}
+	}
+	return occurences
+}
+
+func calcSimilarityScore(occurences map[int]int) int {
+	var score int
+	for k, v := range occurences {
+		score += k * v
+	}
+	return score
+}
+
+func part1(data LocationList) int {
+	sortLocations(data)
+	return sumLocationDistances(data)
+}
+
+func part2(data LocationList) int {
+	return calcSimilarityScore(countBothOccurences(data))
+}
+
 func main() {
 	data, err := readLocations("./input.txt")
 	if err != nil {
 		panic(err)
 	}
-	sortLocations(data)
-	result := sumLocationDistances(data)
-	fmt.Println("Result: ", result)
+	fmt.Printf("Result (part 1): %d\nResult (part2): %d\n", part1(data), part2(data))
 }
